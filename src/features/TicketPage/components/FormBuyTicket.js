88d2f0b88317c2d46a8f387e754504ff07201191
Denis Wolter <wolter.important@gmail.com>
2022-11-28 21:07:19 +0300

    add system BuyTicket

--- src/features/TicketPage/components/FormBuyTicket.js
+++ src/features/TicketPage/components/FormBuyTicket.js
@@ -4,9 +4,12 @@ import {useEffect, useState} from "react";
 const FormBuyTicket = () => {
   const [flights, setFlights] = useState([]);
   const [selectedFlight, setFlight] = useState(-1)
-  const [isTicketBought, setTicketBought] = useState(true);
+  const [isTicketBought, setTicketBought] = useState(false);
   const [date, setDate] = useState(" ")
   const [time, setTime] = useState(" ")
+  const [formFIO, setFormFIO] = useState("")
+  const [formPass, setFormPass] = useState("")
+  const [numPlace, setNumPlace] = useState("__X")
 
   const changeSelector = (v) => {
     const selectInfo = v.target.value;
@@ -14,6 +17,37 @@ const FormBuyTicket = () => {
     console.log(selectInfo);
     setDate(flights.find(item => item.id === +selectInfo)?.dateDepart)
     setTime(flights.find(item => item.id === +selectInfo)?.timeDepart)
+  }
+
+  const buyTicketFoo = async () => {
+    const token = localStorage.getItem("authToken")
+    await fetch("http://localhost:3000/tickets", {
+      method: 'POST',
+      body: JSON.stringify({
+        "FIO_pass": formFIO,
+        "flight": selectedFlight,
+        "login": localStorage.getItem("authToken"),
+        "numPass": formPass,
+      }),
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Authorization': `Bearer ${token}`
+      },
+    })
+      .then(res => res.json())
+      .then(result => {
+          // if (result.msg) return setClientErr(result.msg);
+          // setClientErr("");
+          // setFormVisible(false);
+          // clearFields();
+          // updateTickets()
+          console.log(result);
+          setNumPlace(result.numPlace)
+          setTicketBought(true);
+        }, (errServer) => {
+          console.log(errServer);
+        }
+      )
   }
 
   useEffect(() => {
@@ -32,36 +66,45 @@ const FormBuyTicket = () => {
     )
   })
 
+  function buyOtherTicket() {
+    setTicketBought(false);
+    setFormFIO("");
+    setFormPass("");
+    setNumPlace("__X");
+  }
+
   return (
     <div className="formTicket">
       <div className="inputContainer topLeft">
         <p className="inputText">ФИО</p>
-        <input className="inputField"/>
+        <input disabled={isTicketBought} onChange={v => setFormFIO(v.target.value)} value={formFIO}
+               className="inputField"/>
       </div>
       <div className="inputContainer topRight">
         <p className="inputText">Серия и № паспорта</p>
-        <input className="inputField"/>
+        <input disabled={isTicketBought} onChange={v => setFormPass(v.target.value)} value={formPass}
+               className="inputField"/>
       </div>
       <div className="inputContainer centerLeft smallContainer">
         <p className="inputText">Номер места</p>
-        <p className="placeNumber">__X</p>
+        <p className="placeNumber">{numPlace}</p>
       </div>
-        {
-          (isTicketBought) ?
-            (
-              <button disabled className="inputContainer centerRight smallContainer buttonBuy buttonBuyGreen">
-                <p className="textBuy">Билет успешно куплен</p>
-              </button>
-            )
-            :
-            (<button className="inputContainer centerRight smallContainer buttonBuy">
-              <p className="textBuy">Купить</p>
-            </button>)
-        }
+      {
+        (isTicketBought) ?
+          (
+            <button disabled className="inputContainer centerRight smallContainer buttonBuy buttonBuyGreen">
+              <p className="textBuy">Билет успешно куплен</p>
+            </button>
+          )
+          :
+          (<button onClick={() => buyTicketFoo()} className="inputContainer centerRight smallContainer buttonBuy">
+            <p className="textBuy">Купить</p>
+          </button>)
+      }
       <p className="inputText textUnderline">Информация о рейсе</p>
       <div className="inputContainer bottomLeft">
         <p className="inputText">Рейс</p>
-        <select onChange={(v) => changeSelector(v)} className="inputField selectorFlights">
+        <select disabled={isTicketBought} onChange={(v) => changeSelector(v)} className="inputField selectorFlights">
           <option value={-1}>--- Выбор рейса ---</option>
           {resFlights}
         </select>
@@ -74,6 +117,13 @@ const FormBuyTicket = () => {
         <p className="inputText">Время</p>
         <p className="inputField">{time}</p>
       </div>
+      {
+        isTicketBought &&
+        <button onClick={() => buyOtherTicket()} className="buttonPlus">
+          <img className="buttonNewTicket" src="/img/plus.png" alt="Новый билет"/>
+        </button>
+      }
+
     </div>
   )
 }
